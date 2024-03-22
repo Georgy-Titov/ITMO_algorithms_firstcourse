3edb5ec99b31c1594d2b809aad1c35f94d0d4bb7
Titov Georgy <tgk12072005@gmail.com>
2024-03-22 20:32:47 +0300

    added LAB1.1

---
+++ LAB1.1.py
@@ -0,0 +1,27 @@
+a = [5, 9, 13, 23, 27, 51, 61, 77, 84, 96]
+n = int(input())
+
+
+def binary_search(ls, search):
+    left = 0
+    right = len(a) - 1
+    mid = (left + right) // 2
+
+    steps = 0
+
+    while search != ls[mid] and left < right:
+        steps += 1
+        if search > ls[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+        mid = (left + right) // 2
+
+    if search == ls[mid]:
+        print(f"ID: {mid} Search: {search} Steps: {steps}\n")
+    else:
+        print("Number was not found!\n")
+
+
+binary_search(a, n)
