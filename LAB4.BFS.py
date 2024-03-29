12a53fd8e8ce71d15a76ccfa0be191a1def0d123
Titov Georgy <tgk12072005@gmail.com>
2024-03-29 10:53:08 +0300

    added LAB4.BFS

--- LAB4.BFS.py
+++ LAB4.BFS.py
@@ -1,31 +1,42 @@
 from collections import deque
 
 
-def bfs(start_point, search_point, graph):
-    search_queue = deque()
-    search_queue += graph[start_point]
-    visited = []
-    while search_queue:
-        vertex = search_queue.popleft()
-        if vertex not in visited:
-            if vertex == search_point:
-                print("Found!")
-                return True
-            else:
-                search_queue += graph[vertex]
-                visited.append(vertex)
-    print("Not found!")
-    return False
+def bfs(start, goal, graph):
+    queue = deque([start])
+    visited = {start: start}
+
+    while queue:
+        cur_node = queue.popleft()
+        if cur_node == goal:
+            break
+        next_nodes = graph[cur_node]
+        for next_node in next_nodes:
+            if next_node not in visited:
+                queue.append(next_node)
+                visited[next_node] = cur_node
+
+    if goal not in visited.keys():
+        print(f"No path from {start} to {goal}!")
+    else:
+        print(f"Path from {g} to {s}:\n{g}", end='')
+        while goal != start:
+            goal = visited[goal]
+            print(f" <--- {goal}", end='')
+
+    return 0
 
 
-graph = {"A": ["B", "C"],
-         "B": ["D"],
-         "C": ["E"],
-         "D": ["C"],
-         "E": ["G"],
-         "F": ["C", "E"],
-         "G": ["E", "F", "H"],
-         "H": ["K"],
-         "K": ["G", "H"]}
+a = {"A": ["B", "C"],
+     "B": ["D"],
+     "C": ["E"],
+     "D": ["C"],
+     "E": ["G"],
+     "F": ["C", "E"],
+     "G": ["E", "F", "H"],
+     "H": ["K"],
+     "K": ["G", "H"]}
 
-bfs("G", "A", graph)
+
+s = "A"
+g = "F"
+result = bfs(s, g, a)
